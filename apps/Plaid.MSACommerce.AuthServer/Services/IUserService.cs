ef5e24568f1472649387d7f6622f62405f41cf33
wzh1999 <1973277589@qq.com>
2025-02-20 16:26:02 +0800

    增加注释

--- apps/Plaid.MSACommerce.AuthServer/Services/IUserService.cs
+++ apps/Plaid.MSACommerce.AuthServer/Services/IUserService.cs
@@ -6,6 +6,7 @@ namespace Plaid.MSACommerce.AuthServer.Services
     {
         /// <summary>
         /// 定义Refit请求的接口信息和模式,类似于httpclient操作
+        /// 根据Get标记的路由进行请求不会将方法名称做为最后一层 http地址/api/user?usernam=xxx&password=xxx会命中user控制器中的get方法并接受这两个参数的api接口
         /// </summary>
         /// <param name="username"></param>
         /// <param name="password"></param>
