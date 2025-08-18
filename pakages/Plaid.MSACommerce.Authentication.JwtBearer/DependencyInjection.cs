0336e16838bf79e28b84977861c521c47a46a726
wzh1999 <1973277589@qq.com>
2025-08-18 19:17:24 +0800

    增加代码注释 主要关于鉴权验证处理逻辑

--- pakages/Plaid.MSACommerce.Authentication.JwtBearer/DependencyInjection.cs
+++ pakages/Plaid.MSACommerce.Authentication.JwtBearer/DependencyInjection.cs
@@ -20,7 +20,7 @@ public static class DependencyInjection
         var configurationSection=configuration.GetSection("JwtBearer");
         var jwtSettings = configurationSection.Get<JwtSettings>();
         if (jwtSettings is null) throw new NullReferenceException(nameof(jwtSettings));
-        //注入依赖注入中
+        //注入依赖注入中-通过IOption依赖注入配置类，保好类型安全、可测试、易维护
         services.Configure<JwtSettings>(configurationSection);
 
         services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
@@ -28,11 +28,16 @@ public static class DependencyInjection
             {
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
+                    //取消五分钟容错率
                     ClockSkew = TimeSpan.Zero,
+                    //验证签发者
                     ValidateIssuer = true,
+                    //验收受众
                     ValidateAudience = true,
+                    //验证是否过期
                     ValidateLifetime = true,
                     ValidateIssuerSigningKey = true,
+                    //用于签名的秘钥
                     ValidIssuer = jwtSettings.Issuer,
                     IssuerSigningKey = new SymmetricSecurityKey(
                         Encoding.UTF8.GetBytes(jwtSettings.Secret)
