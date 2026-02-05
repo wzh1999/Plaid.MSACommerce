2dfe2253d48db281703885da1b697b4cd37ded57
wzh1999 <1973277589@qq.com>
2026-02-05 18:54:51 +0800

    增加注释

--- pakages/Plaid.MSACommerce.CommonServiceClient/LoadBalancer.cs
+++ pakages/Plaid.MSACommerce.CommonServiceClient/LoadBalancer.cs
@@ -2,12 +2,15 @@
 using Plaid.MSACommerce.CommonServiceClient.Strategies;
 
 namespace Plaid.MSACommerce.CommonServiceClient;
-
 /// <summary>
 /// 负载均衡器实现类
 /// </summary>
+/// <param name="strategy">负载均衡策略枚举</param>
 public class LoadBalancer(LoadBalancingStrategy strategy) : ILoadBalancer
 {
+    /// <summary>
+    /// 通过switch语法糖判断一下
+    /// </summary>
     private readonly ILoadBalancingStrategy _strategy = strategy switch
     {
         LoadBalancingStrategy.Random => new RandomStrategy(),
