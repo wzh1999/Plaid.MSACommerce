7fe22a8ee7ef406d02e19c641a7cc485aa94e8c8
wzh1999 <1973277589@qq.com>
2026-03-12 19:59:01 +0800

    远程服务调用

--- apps/Plaid.MSACommerce.AuthServer/DependencyInjection.cs
+++ apps/Plaid.MSACommerce.AuthServer/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Plaid.MSACommerce.AuthServer.Services;
+﻿using Plaid.MSACommerce.AuthServer.Clients;
+using Plaid.MSACommerce.AuthServer.Services;
+using Plaid.MSACommerce.CommonServiceClient;
 using Refit;
 
 namespace Plaid.MSACommerce.AuthServer
@@ -7,7 +9,6 @@ namespace Plaid.MSACommerce.AuthServer
     {
         public static IServiceCollection AddHttpApi(this IServiceCollection services, IConfiguration configuration)
         {
-
             ConfigureUserService(services, configuration);
             ConfigureIdentity(services, configuration);
             return services;
@@ -15,15 +16,20 @@ namespace Plaid.MSACommerce.AuthServer
 
         private static void ConfigureUserService(IServiceCollection services, IConfiguration configuration)
         {
-            //读取请求的url地址
-            var userServiceUrl = configuration["ServiceUrls:UserService"];
-            if (userServiceUrl is null) throw new NullReferenceException(nameof(userServiceUrl));
-            //通过HttpClientFactory配置BaseAddress地址,也就是请求url地址
-            services.AddRefitClient<IUserService>()
-               .ConfigureHttpClient(clinet =>
-               {
-                   clinet.BaseAddress = new Uri(userServiceUrl);
-               });
+            #region 绑定注册Refit的Http客户端
+
+            // //读取请求的url地址
+            // var userServiceUrl = configuration["ServiceUrls:UserService"];
+            // if (userServiceUrl is null) throw new NullReferenceException(nameof(userServiceUrl));
+            // //通过HttpClientFactory配置BaseAddress地址,也就是请求url地址
+            // services.AddRefitClient<IUserService>()
+            //     .ConfigureHttpClient(clinet => { clinet.BaseAddress = new Uri(userServiceUrl); });
+
+            #endregion
+            //通用Http请求远程调用服务注入
+            services.AddServiceClient<UserServiceClient>(
+                option => { option.LoadBalancingStrategy = LoadBalancingStrategy.RoundRobin; },
+                client => { client.Timeout = TimeSpan.FromSeconds(1); });
         }
 
         private static void ConfigureIdentity(IServiceCollection services, IConfiguration configuration)
