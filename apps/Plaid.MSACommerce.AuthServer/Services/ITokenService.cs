ee5cfa06f81f1adb9e916f31fc38a0c0e8ec6c83
wzh1999 <1973277589@qq.com>
2026-08-06 20:32:08 +0800

    增加刷新token 认证token接口和服务

---
+++ apps/Plaid.MSACommerce.AuthServer/Services/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace Plaid.MSACommerce.AuthServer.Services;
+/// <summary>
+/// 令牌服务接口 颁发 刷新 授权令牌接口
+/// </summary>
+public interface ITokenService
+{
+    string GenerateAccessToken(IEnumerable<Claim> claims);
+    string GenerateRefreshToken();
+    ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+}
---
+++ apps/Plaid.MSACommerce.AuthServer/Services/TokenService.cs
@@ -0,0 +1,65 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Plaid.MSACommerce.AuthServer.Services;
+
+/// <summary>
+/// 令牌服务接口 颁发 刷新 授权令牌服务
+/// </summary>
+/// <param name="jwtSettings"></param>
+public class TokenService(IOptions<JwtSettings> jwtSettings) : ITokenService
+{
+    public string GenerateAccessToken(IEnumerable<Claim> claims)
+    {
+        var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Value.Secret));
+        var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+        var jwt = new JwtSecurityToken(
+            jwtSettings.Value.Issuer,
+            jwtSettings.Value.Audience,
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(jwtSettings.Value.AccessTokenExpirationMinutes),
+            signingCredentials: signinCredentials);
+
+        var tokenString = new JwtSecurityTokenHandler().WriteToken(jwt);
+        return tokenString;
+    }
+
+    public string GenerateRefreshToken()
+    {
+        var randomNumber = new byte[32];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+        return Convert.ToBase64String(randomNumber);
+    }
+
+    public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+    {
+        var tokenValidationParameters = new TokenValidationParameters
+        {
+            ClockSkew = TimeSpan.Zero,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings.Value.Issuer,
+            ValidAudience = jwtSettings.Value.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(jwtSettings.Value.Secret)
+            )
+        };
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var securityToken);
+        var jwtSecurityToken = securityToken as JwtSecurityToken;
+        if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256,
+                StringComparison.InvariantCultureIgnoreCase))
+        {
+            throw new SecurityTokenException("Token无效");
+        }
+
+        return principal;
+    }
+}
