8b225f6d8f7c8921b49d5d9e64bb4ffe67099c05
wzh1999 <1973277589@qq.com>
2026-08-19 17:27:18 +0800

    增加注释

--- apps/Plaid.MSACommerce.AuthServer/Services/ITokenService.cs
+++ apps/Plaid.MSACommerce.AuthServer/Services/ITokenService.cs
@@ -1,12 +1,29 @@
 ﻿using System.Security.Claims;
 
 namespace Plaid.MSACommerce.AuthServer.Services;
+
 /// <summary>
 /// 令牌服务接口 颁发 刷新 授权令牌接口
 /// </summary>
 public interface ITokenService
 {
+    /// <summary>
+    /// 获取认证token
+    /// </summary>
+    /// <param name="claims">自定义用户信息</param>
+    /// <returns></returns>
     string GenerateAccessToken(IEnumerable<Claim> claims);
+
+    /// <summary>
+    /// 获取刷新令牌
+    /// </summary>
+    /// <returns></returns>
     string GenerateRefreshToken();
+
+    /// <summary>
+    /// 根据过期token获取用户详细信息
+    /// </summary>
+    /// <param name="token"></param>
+    /// <returns></returns>
     ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
 }
--- apps/Plaid.MSACommerce.AuthServer/Services/TokenService.cs
+++ apps/Plaid.MSACommerce.AuthServer/Services/TokenService.cs
@@ -13,6 +13,11 @@ namespace Plaid.MSACommerce.AuthServer.Services;
 /// <param name="jwtSettings"></param>
 public class TokenService(IOptions<JwtSettings> jwtSettings) : ITokenService
 {
+    /// <summary>
+    /// 生成访问令牌
+    /// </summary>
+    /// <param name="claims">自定义token中信息</param>
+    /// <returns></returns>
     public string GenerateAccessToken(IEnumerable<Claim> claims)
     {
         var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Value.Secret));
@@ -27,7 +32,10 @@ public class TokenService(IOptions<JwtSettings> jwtSettings) : ITokenService
         var tokenString = new JwtSecurityTokenHandler().WriteToken(jwt);
         return tokenString;
     }
-
+    /// <summary>
+    /// 生成刷新令牌
+    /// </summary>
+    /// <returns></returns>
     public string GenerateRefreshToken()
     {
         var randomNumber = new byte[32];
@@ -36,6 +44,12 @@ public class TokenService(IOptions<JwtSettings> jwtSettings) : ITokenService
         return Convert.ToBase64String(randomNumber);
     }
 
+    /// <summary>
+    /// 过期令牌中获取用户详细信息
+    /// </summary>
+    /// <param name="token">过期token</param>
+    /// <returns></returns>
+    /// <exception cref="SecurityTokenException"></exception>
     public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
     {
         var tokenValidationParameters = new TokenValidationParameters
